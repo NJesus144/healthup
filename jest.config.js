d6bb4d817e4215b3dee9322dc01dddc1ea22c862
NJesus144 <nalbert-edm@outlook.com>
2025-06-19 18:34:14 -0300

    feat: initialize project with TypeScript, Express, and Prisma setup

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      tsconfig: 'tsconfig.json'
+    }],
+  }
+}
