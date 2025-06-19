668a5f007f0e1a55d51e649a321296ca2b5330b0
NJesus144 <nalbert-edm@outlook.com>
2025-06-19 18:35:49 -0300

    feat: implement PatientController with create, profile, and update methods

---
+++ src/modules/patients/controllers/PatientController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from 'express'
+import { PatientService } from '@/interfaces/services/PatientService'
+import { validateCreatePatient } from '@/modules/patients/validators/validateCreatePatient'
+import { responseSuccess } from '@/shared/helpers/responseSuccess'
+import { validateId } from '@/modules/patients/validators/validateId'
+import { validateUpdatePatient } from '@/modules/patients/validators/validateUpdatePatient'
+
+export class PatientController {
+  constructor(private readonly patientService: PatientService) {}
+
+  async create(req: Request, res: Response): Promise<Response> {
+    const data = validateCreatePatient(req.body)
+
+    const patient = await this.patientService.createPatient(data)
+
+    return responseSuccess(res, patient, 'Patient created successfully', 201)
+  }
+
+  async profile(req: Request, res: Response): Promise<Response> {
+    const patientId = validateId(req.body.id)
+
+    const patient = await this.patientService.getPatientById(patientId)
+
+    return responseSuccess(res, patient, 'Patient profile retrieved successfully')
+  }
+
+  async update(req: Request, res: Response): Promise<Response> {
+   const patientId = validateId(req.params.id)
+   const data = validateUpdatePatient(req.body)
+  
+   const patient = await this.patientService.updatePatient(patientId, data)
+
+   return responseSuccess(res, patient, 'Patient updated successfully')
+  }
+}
