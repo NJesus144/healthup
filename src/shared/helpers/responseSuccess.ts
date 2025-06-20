cb55541adaa15c6dff8b0fe8f9b6cc73e30bed8b
NJesus144 <nalbert-edm@outlook.com>
2025-06-20 04:33:30 -0300

    add responseSuccess helper function for standardized success responses

---
+++ src/shared/helpers/responseSuccess.ts
@@ -0,0 +1,14 @@
+import { Response } from 'express'
+
+export function responseSuccess(
+  res: Response,
+  data: any,
+  message = "Success",
+  status = 200,
+) {
+  return res.status(status).json({
+    success: true,
+    message,
+    data,
+  })
+}
