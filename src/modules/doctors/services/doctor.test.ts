d75b859d95f04726c9edc5791c754ffc92d279a5
NJesus144 <nalbert-edm@outlook.com>
2025-06-22 16:54:21 -0300

    test: enhance DoctorService tests with additional scenarios for email, CPF validation, and retrieval methods

--- src/modules/doctors/services/doctor.test.ts
+++ src/modules/doctors/services/doctor.test.ts
@@ -1,5 +1,5 @@
 import { FakeDoctorRepository } from '@/interfaces/repositories/fake_doctor_repository'
-import { DoctorServiceImp } from '@/modules/doctors/services/DoctorService'
+import { DoctorServiceImp } from '@/modules/doctors/services/DoctorServiceImp'
 import { MedicalSpecialty, UserStatus } from '@prisma/client'
 
 describe('Doctor Service', () => {
@@ -12,14 +12,60 @@ describe('Doctor Service', () => {
   })
 
   describe('createDoctor', () => {
+    it('should return 409 when trying to create a doctor with an existing email', async () => {
+      const existingDoctorData = {
+        name: 'João Silva',
+        email: 'drjoaoSilva@gmail.com',
+        passwordHash: 'senha123',
+        phone: '11999999999',
+        specialty: MedicalSpecialty.CARDIOLOGY,
+        status: UserStatus.PENDING,
+        cpf: '44180672057',
+        crm: '123456-SP',
+      }
+
+      await expect(doctorService.createDoctor(existingDoctorData)).rejects.toThrow('Email already exists')
+    })
+
+    it('should return ConflictError 409 when trying to create a doctor with an existing CPF', async () => {
+      const existingDoctorCPF = {
+        name: 'João Silva',
+        email: 'drjoaoSilva@gmail.com',
+        passwordHash: 'senha123',
+        phone: '11999999999',
+        specialty: MedicalSpecialty.CARDIOLOGY,
+        status: UserStatus.PENDING,
+        cpf: '38823074045',
+        crm: '123456-SP',
+      }
+
+      await expect(doctorService.createDoctor(existingDoctorCPF)).rejects.toThrow('CPF already exists')
+    })
+
+    it('should return 400 when trying to create a doctor with an invalid CPF', async () => {
+      const existingDoctorCPF = {
+        name: 'João Silva',
+        email: 'drjoaoSilva@gmail.com',
+        passwordHash: 'senha123',
+        phone: '11999999999',
+        specialty: MedicalSpecialty.CARDIOLOGY,
+        status: UserStatus.PENDING,
+        cpf: '23453452-456',
+        crm: '123456-SP',
+      }
+
+      await expect(doctorService.createDoctor(existingDoctorCPF)).rejects.toThrow('Invalid CPF format')
+    })
+
     it('should create a doctor with valid data', async () => {
       const existingDoctorData = {
         name: 'Dr. João Silva',
-        email: 'drjoaoSilva@gmail.com',
+        email: 'drjoaoSilva2@gmail.com',
         passwordHash: 'senha123',
-        specialty: MedicalSpecialty.CARDIOLOGIA,
+        phone: '11999999999',
+        specialty: MedicalSpecialty.CARDIOLOGY,
         status: UserStatus.PENDING,
-        cpf: '12345678901',
+        cpf: '11942527020',
         crm: '123456-SP',
       }
 
@@ -31,7 +77,51 @@ describe('Doctor Service', () => {
       expect(createdDoctor.specialty).toBe(existingDoctorData.specialty)
       expect(createdDoctor.status).toBe(existingDoctorData.status)
       expect(createdDoctor.cpf).toBe(existingDoctorData.cpf)
-      expect(createdDoctor.crm).toBe(existingDoctorData.crm)
+    })
+  })
+
+  describe('getDoctorById', () => {
+    it('should throw NotFoundError when no doctor is found', async () => {
+      await expect(doctorService.getDoctorById('non-existing-id')).rejects.toThrow('User not found')
+    })
+
+    it('should return a doctor when found', async () => {
+      const doctor = await doctorService.getDoctorById('1')
+
+      expect(doctor).not.toBeNull()
+      expect(doctor?.id).toBe('1')
+      expect(doctor?.name).toBe('Dr. João Silva')
+    })
+  })
+
+  describe('updateDoctor', () => {
+    it('should throw NotFoundError when trying to update a non-existent doctor', async () => {
+      const doctorData = {
+        name: 'new name',
+      }
+
+      await expect(doctorService.updateDoctor('12009', doctorData)).rejects.toThrow('User not found')
+    })
+
+    it('should successfully update an existing doctor', async () => {
+      const updateDate = {
+        name: 'Updated Name',
+      }
+
+      const updatedDoctor = await doctorService.updateDoctor('1', updateDate)
+
+      expect(updatedDoctor).toBeDefined()
+      expect(updatedDoctor.name).toBe(updateDate.name)
+    })
+  })
+
+  describe('getAvailableDoctors', () => {
+    it('should return all available doctors', async () => {
+      const doctors = await doctorService.findAllAvailableDoctors()
+
+      expect(doctors).toBeDefined()
+      expect(doctors.length).toBeGreaterThan(0)
+      expect(doctors[0].name).toBe('Dr. João Silva')
     })
   })
 })
