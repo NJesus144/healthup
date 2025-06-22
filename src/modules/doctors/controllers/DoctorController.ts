0c14e2961e63d0d4a4a3d21346e7612b30584159
NJesus144 <nalbert-edm@outlook.com>
2025-06-22 16:53:27 -0300

    fix: correct import path for validators and update DoctorController methods for consistency

--- src/modules/doctors/controllers/DoctorController.ts
+++ src/modules/doctors/controllers/DoctorController.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from 'express'
-import { validateCreateDoctor } from '@/modules/doctors/vlidators/validateCreateDoctor'
 import { DoctorService } from '@/interfaces/services/DoctorService'
 import { responseSuccess } from '@/shared/helpers/responseSuccess'
+import { AuthenticatedRequest } from '@/shared/middlewares/authenticationMiddleware'
+import { validateCreateDoctor } from '@/modules/doctors/validators/validateCreateDoctor'
+import { validateUpdateDoctor } from '@/modules/doctors/validators/validateUpdateDoctor'
+import { validateGetDoctorsQuery } from '@/modules/doctors/validators/validateQueryParameters'
 
 export class DoctorController {
   constructor(private readonly doctorService: DoctorService) {}
@@ -9,8 +12,32 @@ export class DoctorController {
   async create(req: Request, res: Response): Promise<Response> {
     const data = validateCreateDoctor(req.body)
 
-    const patient = await this.doctorService.createDoctor(data)
+    const doctor = await this.doctorService.createDoctor(data)
 
-    return responseSuccess(res, patient, 'Doctor created successfully', 201)
+    return responseSuccess(res, doctor, 'Doctor created successfully', 201)
+  }
+
+  async me(req: AuthenticatedRequest, res: Response): Promise<Response> {
+    const doctorId = req.user!.sub
+
+    const doctor = await this.doctorService.getDoctorById(doctorId)
+
+    return responseSuccess(res, doctor, 'Doctor profile retrieved successfully')
+  }
+
+  async update(req: AuthenticatedRequest, res: Response): Promise<Response> {
+    const doctorId = req.user!.sub
+    const data = validateUpdateDoctor(req.body)
+
+    const doctor = await this.doctorService.updateDoctor(doctorId, data)
+
+    return responseSuccess(res, doctor, 'Doctor updated successfully')
+  }
+
+  async getAllDoctors(req: Request, res: Response): Promise<Response> {
+    const queryDto = validateGetDoctorsQuery(req.query)
+    const doctors = await this.doctorService.findAllAvailableDoctors(queryDto)
+
+    return responseSuccess(res, doctors, 'Doctors retrieved successfully')
   }
 }
