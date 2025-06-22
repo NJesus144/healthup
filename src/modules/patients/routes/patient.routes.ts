24b6078d7aa95f00b94b477f9a34722f99e48b5a
NJesus144 <nalbert-edm@outlook.com>
2025-06-22 16:56:59 -0300

    refactor: streamline patient routes by removing unused imports and adjusting authentication strategy

--- src/modules/patients/routes/patient.routes.ts
+++ src/modules/patients/routes/patient.routes.ts
@@ -1,23 +1,22 @@
-import { Request, Response, Router } from 'express'
+import { Response, Router } from 'express'
 import { PatientController } from '@/modules/patients/controllers/PatientController'
 import { asyncHandler, errorHandler } from '@/shared/errors/errorHandler'
 import { PatientServiceImp } from '@/modules/patients/services/PatientServiceImp'
-// import { InMemoryPatientRepository } from '@/modules/patients/repositories/InMemoryPatientRepository'
 import { AuthenticationServiceImp } from '@/modules/authentication/services/AuthenticationServiceImp'
 import { AuthenticatedRequest, createAuthMiddleware } from '@/shared/middlewares/authenticationMiddleware'
 import { PatientRepositoryImp } from '@/modules/patients/repositories/PatientRepository'
+import { PatientAuthenticationStrategy } from '@/interfaces/auth/PatientAuthenticationStrategy'
 
 const router = Router()
 
 const patientRepository = new PatientRepositoryImp()
 const patientService = new PatientServiceImp(patientRepository)
-const authenticationService = new AuthenticationServiceImp(patientService)
+const patientAuthStrategy = new PatientAuthenticationStrategy(patientService)
+const authenticationService = new AuthenticationServiceImp(patientAuthStrategy)
 const patientController = new PatientController(patientService)
 const authenticationMiddleware = createAuthMiddleware(authenticationService)
 
 router.post('/', asyncHandler(patientController.create.bind(patientController)))
-
-// router.get('/profile/:id', authenticationMiddleware, (req: AuthenticatedRequest, res: Response) => patientController.profile(req, res))
 
 router.get(
   '/me',
@@ -26,7 +25,7 @@ router.get(
 )
 
 router.put(
-  '/:id',
+  '/',
   authenticationMiddleware,
   asyncHandler((req: AuthenticatedRequest, res: Response) => patientController.update(req, res))
 )
