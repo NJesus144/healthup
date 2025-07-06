cf43117ff9d8930d08e63d2a006adf5d675260ad
NJesus144 <nalbert-edm@outlook.com>
2025-07-06 08:25:55 -0300

    feat: update DoctorRepository and DoctorService interfaces to include blocked date management methods

--- src/interfaces/repositories/DoctorRepository.ts
+++ src/interfaces/repositories/DoctorRepository.ts
@@ -1,13 +1,20 @@
+import { CreateBloquedDateDTO } from '@/modules/doctors/dtos/CreateBloquedDateDTO'
 import { CreateDoctorDTO } from '@/modules/doctors/dtos/CreateDoctorDTO'
+import { UpdateDoctorDTO } from '@/modules/doctors/dtos/UpdateDoctorDTO'
 import { Doctor } from '@/modules/doctors/models/Doctor'
 import { PrismaDoctor } from '@/modules/doctors/repositories/DoctorRepositoryImp'
 import { GetDoctorsQueryDTO } from '@/modules/doctors/validators/validateQueryParameters'
+import { BlockedDate } from '@prisma/client'
 
 export interface DoctorRepository {
   createDoctor(createDoctorDTO: CreateDoctorDTO): Promise<Doctor>
   getDoctorById(id: string): Promise<Doctor | null>
-  updateDoctor(id: string, createDoctorDTO: CreateDoctorDTO): Promise<Doctor>
+  updateDoctor(id: string, updateDoctorDTO: UpdateDoctorDTO): Promise<Doctor>
   findDoctorByCPF(cpf: string): Promise<Doctor | null>
   getDoctorByEmail(email: string): Promise<PrismaDoctor | null>
   findAllAvailableDoctors(filter: GetDoctorsQueryDTO): Promise<Doctor[]>
+  blockedDate(doctorId: string, date: CreateBloquedDateDTO): Promise<BlockedDate>
+  getBlockedDates(doctorId: string, startDate: Date, endDate: Date): Promise<Date[]>
+  cancelBlockedDate(doctorId: string, date: Date): Promise<BlockedDate>
+  getAllBlockedDates(doctorId: string): Promise<Date[]>
 }
